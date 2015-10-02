a6dbcb1ac81c46a42a28d0056dc3ec13de18a866
zhezhangsh <zhangz@email.chop.edu>
2015-10-02 01:08:53 -0400

    de test

---
+++ export_functions.r
@@ -0,0 +1,23 @@
+
+# Export functions in NAMESPACE file
+path<-'./R';
+fn<-paste(path, dir(path), sep='/');
+fn<-fn[grep('.r$', fn, ignore.case=TRUE)];
+if (length(fn)>0) {
+	fnc<-lapply(fn, function(fn) {
+		ln<-scan(fn, what='', flush=TRUE, sep='\n');
+    ln<-ln[!grepl('^ ', ln)];
+		ln<-gsub(' ', '', ln);
+		ln<-ln[grep("<-function\\(", ln)];
+		sapply(strsplit(ln, "<-function\\("), function(x) x[1]);
+	});
+	fnc<-sort(as.vector(unlist(fnc)));
+  ln<-sort(paste('export("', fnc, '");', sep=''));
+  writeLines(c('', ln, ''), paste(Sys.getenv('AWSOMICS_HOME'), 'NAMESPACE', sep='/'));
+}
+
+# Loading function script
+ln<-c(paste('AWSOMICS_HOME<-Sys.getenv("AWSOMICS_HOME");', sep=''), 'library(devtools);');
+ln<-c(ln, paste('source_url("https://raw.githubusercontent.com/zhezhangsh/rchive/master/', sub('./', '', fn), '");', sep=''));
+writeLines(c('', ln, ''), file(paste(Sys.getenv('AWSOMICS_HOME'), 'load_by_url.r', sep='/')));
+
