5300f01b429dbdb72c71a2df1b1ac4ad3ab49ef7
zhezhangsh <zhangz@email.chop.edu>
2016-02-16 16:40:15 -0500

    update

--- R/CreateDeReport.R
+++ R/CreateDeReport.R
@@ -1,34 +1,30 @@
 # Use this template to create a report of differential gene expression 
-CreateDeReport<-function(inputs, is.url=TRUE, dir.temp='.',
-                         rmd="https://raw.githubusercontent.com/zhezhangsh/DEGandMore/master/examples/DeReport/DeReport.Rmd") {
-  # inputs    A list of input variables, or path to the file
-  # is.url    If the .Rmd template is an URL; a local file otherwise
-  # rmd       URL or path to the .Rmd template file
+CreateDeReport<-function(yml) {
+  # yml     The yaml file or an yaml list defines the inputs and parameters of the analysis
   
-  if (is.character(inputs)) inputs<-readRDS(inputs);
+  library(awsomics);
+  library(gplots);
+  library(knitr);
+  library(rmarkdown); 
   
-  lb<-strsplit("org.Hs.eg.db; RCurl; formatR; GenomeInfoDb; XVector; methods; bitops; utils; tools; grDevices; zlibbioc; digest; biclust; jsonlite; evaluate; DEGandMore; RSQLite; gage; lattice; png; pathview; graph; KEGGgraph; rstudioapi; DBI; Rgraphviz; curl; yaml; parallel; rJava; knitr; httr; stringr; htmlwidgets; xlsxjars; Biostrings; S4Vectors; graphics; gtools; datasets; stats; caTools; IRanges; DT; stats4; grid; base; Biobase; R6; snow; AnnotationDbi; XML; rmarkdown; gdata; isa2; magrittr; htmltools; gplots; awsomics; modeltools; MASS; BiocGenerics; RankProd; KEGGREST; flexclust; colorspace; xlsx; KernSmooth; stringi", '; ')[[1]];
-  pkgs<-rownames(installed.packages());
-  lb<-lb[!(lb %in% pkgs)];
-  if (length(lb) > 0) stop("Required packages not installed: ", paste(lb, collapse='; '));
+  if (class(yml) == 'character') {
+    if (!file.exists(yml)) stop('Input file', yml, 'not found\n'); 
+    yml <- yaml::yaml.load_file(yml);  
+  }
+
+  if (!file.exists(yml$output)) dir.create(yml$output, recursive = TRUE);
   
-  if (!is.url) {
-    if (!RCurl::url.exists(rmd)) stop("Rmd file not exists: ", rmd);
+  if (yml$input$remote) {
+    if (!RCurl::url.exists(yml$input$template)) stop("Template Rmd file ', yml$input$template, ' not exists\n");
+    writeLines(RCurl::getURL(yml$input$template)[[1]], './DeReport.Rmd');
   } else {
-    ln<-RCurl::getURL(rmd)[[1]];
-    writeLines(ln, paste(dir.temp, 'DeReport.Rmd', sep='/'));
+    file.copy(yml$input$template, './DeReport.Rmd'); 
   }
   
-  g1.ind<-inputs$indexes[[1]];
-  g2.ind<-inputs$indexes[[2]];
-  g1.name<-inputs$names[1];
-  g2.name<-inputs$names[2];
-  g1.name<-gsub('-', '_', g1.name);
-  g2.name<-gsub('-', '_', g2.name);
-  path0<-paste(g1.name, g2.name, sep='-vs-');
-  path<-paste(inputs$output, path0, sep='/');
+  errors<-try(rmarkdown::render('./DeReport.Rmd', output_format="html_document", output_file="index.html", output_dir='.', 
+                                quiet=TRUE, envir=new.env()), silent=TRUE);
   
-  rmarkdown::render(paste(dir.temp, 'DeReport.Rmd', sep='/'), output_file=paste(path, 'index.html', sep='/'));
-  
+  fn.html<-paste(yml$output, 'index.html', sep='/'); 
+
   paste(path, 'index.html', sep='/');
 }
