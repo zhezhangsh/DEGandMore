dfe432389d36f3847b49a1e9b5c04e0027c8ffee
zhezhangsh <zhangz@email.chop.edu>
2016-02-16 22:08:35 -0500

    DE report

--- R/CreateDeReport.R
+++ R/CreateDeReport.R
@@ -6,6 +6,7 @@ CreateDeReport<-function(yml) {
   library(gplots);
   library(knitr);
   library(rmarkdown); 
+  library(yaml);
   
   if (class(yml) == 'character') {
     if (!file.exists(yml)) stop('Input file', yml, 'not found\n'); 
@@ -14,20 +15,22 @@ CreateDeReport<-function(yml) {
 
   if (!file.exists(yml$output)) dir.create(yml$output, recursive = TRUE);
   
+  path<-paste(yml$output, paste(names(yml$input$comparison), collapse='-vs-'), sep='/');
+  if (file.exists(path)) unlink(path, recursive = TRUE); 
+  
   if (yml$input$remote) {
     if (!RCurl::url.exists(yml$input$template)) stop("Template Rmd file ', yml$input$template, ' not exists\n");
     writeLines(RCurl::getURL(yml$input$template)[[1]], './DeReport.Rmd');
   } else {
     file.copy(yml$input$template, './DeReport.Rmd'); 
   }
-  
-  errors<-try(rmarkdown::render('DeReport.Rmd', output_format="html_document", output_file="index.html", output_dir='.', 
+
+  errors<-try(rmarkdown::render('DeReport.Rmd', output_format="html_document", output_file="index.html", output_dir=path, 
                                 quiet=TRUE, envir=new.env()), silent=TRUE);
   
-  path<-paste(yml$output, paste(names(yml$input$comparison), collapse='-vs-'), sep='/');
-
-  fn.html<-paste(path, 'index.html', sep='/'); 
-  file.copy('index.html', fn.html, overwrite = TRUE);
-
-  fn.html; 
+  file.copy('./DeReport.Rmd', paste(path, 'DeReport.Rmd', sep='/'));
+  writeLines(as.yaml(yml), paste(path, 'DeReport.yml', sep='/'));
+  zip(paste(path, '.zip', sep=''), path, flag='-r0X', zip='zip');
+  
+  path; 
 }
