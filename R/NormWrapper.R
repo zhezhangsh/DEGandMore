be58da4aaeb1f469ca5ed6aa653bc031eb4011b2
Zhang <zhangz@email.chop.edu>
2016-06-06 12:26:34 -0400

    normalize

--- R/NormWrapper.R
+++ R/NormWrapper.R
@@ -58,7 +58,7 @@ NormQQ <- function(mtrx, ref=c('mean', 'median', 'first', 'last')) {
         x<-rowMeans(mtrx, na.rm=TRUE);
   x <- rev(sort(x));
   
-  d <- apply(mtrx, 2, function(y) {
+  apply(mtrx, 2, function(y) {
     names(y) <- 1:length(y); 
     z <- y[y > x[length(x)]]; 
     z[1:length(z)] <- x[length(z)-rank(z, ties.method='random')+1]; 
