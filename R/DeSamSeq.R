ce9fbed4189632cf12c12c986939cff79073a1e3
Zhang <zhangz@email.chop.edu>
2016-07-04 12:02:16 -0400

    differential expression

--- R/DeSamSeq.R
+++ R/DeSamSeq.R
@@ -28,10 +28,12 @@ DeSamSeq <- function(mtrx, grps, paired=FALSE, normalization=c('TMM', 'RLE', 'DE
   capture.output(
     if (paired & n[1]==n[2]) {
       y <- c(-(1:n[1]), 1:n[1]);
-      sam <- SAMseq(mtrx, y, resp.type = 'Two class paired', nperms = nperms, nresamp = nresamp)$samr.obj;
+      samfit <- SAMseq(mtrx, y, resp.type = 'Two class paired', nperms = nperms, nresamp = nresamp)
+      sam <- samfit$samr.obj;
     } else {
       y <- c(rep(1, n[1]), rep(2, n[2])); 
-      sam <- SAMseq(mtrx, y, resp.type = 'Two class unpaired', nperms = nperms, nresamp = nresamp)$samr.obj;
+      samfit <- SAMseq(mtrx, y, resp.type = 'Two class unpaired', nperms = nperms, nresamp = nresamp)
+      sam <- samfit$samr.obj;
   }) -> x;
   
   d  <- list(x=mtrx, y=y, geneid=rownames(mtrx), genename=rownames(mtrx), logged2=FALSE); 
@@ -45,5 +47,5 @@ DeSamSeq <- function(mtrx, grps, paired=FALSE, normalization=c('TMM', 'RLE', 'DE
                    'LogFC', 'Pvalue', 'FDR');
   rownames(s) <- rownames(mtrx); 
   
-  list(stat=s[rownames(mtrx), ], group=grps, cd=cd);
+  list(stat=s[rownames(mtrx), ], group=grps, sam=samfit);
 }
