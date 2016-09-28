996f211d9fe35df6419a46bd53063eef0d821b09
Zhang <zhangz@email.chop.edu>
2016-09-28 09:28:35 -0400

    installer

---
+++ installer.r
@@ -0,0 +1,33 @@
+# Install dependency required to run the DE methods
+installed <- rownames(installed.packages()); 
+
+# Install required cran packages
+pkg1 <- c('devtools', 'XML', 'readxl', 'shinythemes');
+pkg  <- setdiff(pkgs, installed);
+if (length(pkg) > 0) {
+  install.packages(pkg); 
+}
+
+# install Bioconductor packages
+pkg2 <- c("ABSSeq", "ALDEx2", "BADER", "baySeq", "BGmix", "bridge", "DEGseq", "DESeq2", "EBSeq", "edgeR", "edgeRun", 
+          "limma", "LMGene", "NOISeq", "plgem", "PoissonSeq", "RankProd", "RBM", "samr", "sSeq", "tweeDEseq"); 
+pkg  <- setdiff(pkg2, installed);
+if (length(pkg) > 0) {
+  source("http://bioconductor.org/biocLite.R");
+  biocLite(pkg); 
+}
+
+# install GitHub packages (force to install to keep)
+pkg2 <- c('RoCA', 'rchive', 'awsomics', 'DEGandMore')
+require(devtools); 
+install_github('zhezhangsh/RoCAR');
+install_github('zhezhangsh/rchive');
+install_github('zhezhangsh/awsomicsR');
+install_github('zhezhangsh/DEGandMore');
+
+# Check if all packages can be loaded
+pkgs   <- c(pkg1, pkg2, pkg3); 
+loaded <- sapply(pkgs, function(pkg) require(pkg, character.only = TRUE));
+if(length(pkgs[!loaded]) == 0) cat("Congratulations! All required packages have been installed and loaded.\n") else
+  cat('Oops!, Some packages are not installed/loaded: ', paste(pkgs[!loaded], collapse='; '), '\n'); 
+
